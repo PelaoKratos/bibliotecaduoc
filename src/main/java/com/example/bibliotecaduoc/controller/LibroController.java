1be3e5e6427b2fd4f4e0334573248c26b7d49f6c
Matias Guerra <mat.guerraj@duocuc.cl>
2026-03-24 12:12:13 -0300

    asd

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.bibliotecaduoc.model.Libro;
 import com.example.bibliotecaduoc.service.LibroService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
+@RequestMapping("/api/v1/libros")
 public class LibroController {
     @Autowired
     private LibroService libroService;
