20e7bdf73699648248e6dad6a141f71247b865eb
Matias Guerra <mat.guerraj@duocuc.cl>
2026-03-24 12:06:04 -0300

    get libros agregador

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -1,5 +1,24 @@
 package com.example.bibliotecaduoc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class LibroController {
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> ListarLibros() {
+        return libroService.readAll();
+    }
     
 }
