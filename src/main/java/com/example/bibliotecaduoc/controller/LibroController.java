e324e1aeb3ef8d9ec4de42e5824523e1729b534e
Matias Guerra <mat.guerraj@duocuc.cl>
2026-03-25 11:27:23 -0300

    get por id agregado

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.bibliotecaduoc.model.Libro;
 import com.example.bibliotecaduoc.service.LibroService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -21,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class LibroController {
     @Autowired
     private LibroService libroService;
-
+    //LO IDEAL ES SIEMPRE EMPEZAR CON EL GET 
     @GetMapping
     public List<Libro> listaLibros() {
         return libroService.readAll();
@@ -32,5 +35,12 @@ public class LibroController {
         libroService.save(libro);
         return libro;
     }
+    
+    //Para buscar solo por id, usa la misma url pero al final se agrega el id que quieres
+    @GetMapping("{id}")
+    public Libro buscaLibro(@PathVariable int id) {
+        return libroService.readbyId(id);
+    }
+
     
 }
