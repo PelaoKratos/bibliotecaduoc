7d4aebee87f1982218ecb24b24d49f45ea36e0bb
Matias Guerra <mat.guerraj@duocuc.cl>
2026-03-25 10:29:29 -0300

    Controller avance

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -1,5 +1,27 @@
 package com.example.bibliotecaduoc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api/v1/libros")
+
 public class LibroController {
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> listaLibros() {
+        return libroService.readAll();
+    }
     
 }
